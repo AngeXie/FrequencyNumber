4e935a99672afd61f0b2273115fe7ae86ece066c
ange <52370ht@sina.com>
2017-04-20 01:08:22 +0800

    status_split+count

--- FrequencyNumberTest.js
+++ FrequencyNumberTest.js
@@ -1,14 +1,36 @@
 /**
  * Created by XAQ on 2017/4/19.
  */
-//分解字符串
-function splitString(string){
-    if(string === "" || string === " ")
-        return [];
-    return string.split(" ");
+function getResult(elemArry,countArry){
+    var result="";
+    for(var i=0;i<elemArry.length;i++){
+        result += elemArry[i] + " " + countArry[i] + "\r\n";
+    }
+    return result;
 }
 function main(string){
-    return splitString(string);
+    if(string === "" || string === " ")
+        return null;
+    var stringArry=string.split(" ");
+    var elemArry = new Array();
+    var countArry = new Array();
+    for(var i=0;i<stringArry.length;i++){
+        if(i === 0){
+            elemArry[i]=stringArry[i];
+            countArry[i]=1;continue;
+        }
+        var j=0;
+        for(;j<stringArry.length;j++){
+            if(stringArry[i] === elemArry[j]){
+                countArry[j]++;break;
+            }
+        }
+        if(j === stringArry.length){
+            elemArry[elemArry.length] = stringArry[i];
+            countArry[countArry.length] = 1;
+        }
+    }
+    return getResult(elemArry,countArry);
 }
 
 module.exports=main;
